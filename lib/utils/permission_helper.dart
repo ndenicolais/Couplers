c1f36d180589be2281f5798446676bc46d641036
ndenicolais <ndn21dev@gmail.com>
2025-03-01 10:25:14 +0100

    Improved the permissions request

--- lib/utils/permission_helper.dart
+++ lib/utils/permission_helper.dart
@@ -26,11 +26,20 @@ Future<void> requestStoragePermission(
           if (context.mounted) {
             showErrorToast(
               context,
-              AppLocalizations.of(context)!.storage_permission_error,
+              AppLocalizations.of(context)!.permission_storage_denied,
             );
           }
+          throw Exception('Storage permission denied');
         } else if (filePermission.isPermanentlyDenied) {
+          if (context.mounted) {
+            showErrorToast(
+              context,
+              AppLocalizations.of(context)!.permission_storage_toast,
+            );
+          }
+          await Future.delayed(const Duration(milliseconds: 1200));
           openAppSettings();
+          throw Exception('Storage permission permanently denied');
         }
       }
     }
@@ -49,15 +58,63 @@ Future<void> requestStoragePermission(
           if (context.mounted) {
             showErrorToast(
               context,
-              AppLocalizations.of(context)!.storage_permission_error,
+              AppLocalizations.of(context)!.permission_storage_toast,
             );
           }
+          throw Exception('Storage permission denied');
         } else if (filePermission.isPermanentlyDenied) {
+          if (context.mounted) {
+            showErrorToast(
+              context,
+              AppLocalizations.of(context)!.permission_storage_toast,
+            );
+          }
+          await Future.delayed(const Duration(milliseconds: 1200));
           openAppSettings();
+          throw Exception('Storage permission permanently denied');
         }
       }
     }
   }
+}
+
+Future<String> requestManageExternalStoragePermission(
+  BuildContext context,
+) async {
+  if (Platform.isAndroid) {
+    PermissionStatus manageExternalStoragePermission =
+        await Permission.manageExternalStorage.status;
+
+    if (manageExternalStoragePermission.isGranted) {
+      return 'Permission granted';
+    } else {
+      manageExternalStoragePermission =
+          await Permission.manageExternalStorage.request();
+
+      if (manageExternalStoragePermission.isGranted) {
+        return 'Permission granted';
+      } else if (manageExternalStoragePermission.isDenied) {
+        if (context.mounted) {
+          showErrorToast(
+            context,
+            AppLocalizations.of(context)!.permission_storage_denied,
+          );
+        }
+        return 'Permission denied';
+      } else if (manageExternalStoragePermission.isPermanentlyDenied) {
+        if (context.mounted) {
+          showErrorToast(
+            context,
+            AppLocalizations.of(context)!.permission_storage_toast,
+          );
+        }
+        await Future.delayed(const Duration(milliseconds: 1200));
+        openAppSettings();
+        throw Exception('Storage permission permanently denied');
+      }
+    }
+  }
+  throw Exception('Unsupported platform');
 }
 
 Future<void> requestLocationPermission(
@@ -70,10 +127,19 @@ Future<void> requestLocationPermission(
     if (context.mounted) {
       showErrorToast(
         context,
-        AppLocalizations.of(context)!.location_permission_error,
+        AppLocalizations.of(context)!.permission_location_denied,
       );
     }
+    throw Exception('Location permission denied');
   } else if (locationPermission.isPermanentlyDenied) {
+    if (context.mounted) {
+      showErrorToast(
+        context,
+        AppLocalizations.of(context)!.permission_location_toast,
+      );
+    }
+    await Future.delayed(const Duration(milliseconds: 1200));
     openAppSettings();
+    throw Exception('Location permission permanently denied');
   }
 }
